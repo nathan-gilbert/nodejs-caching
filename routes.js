2b42c32032b4ebd480a1991f947666f70a2095c3
Nathan Gilbert <nathan.gilbert@gmail.com>
2024-03-07 10:38:07 -0700

    Add user data

--- routes.js
+++ routes.js
@@ -1,5 +1,17 @@
+const fs = require("node:fs");
+
+function readData() {
+  try {
+    const data = fs.readFileSync("data.txt", "utf8");
+    console.log(data);
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
 async function routes(fastify, options) {
-  fastify.get("/", async (request, reply) => {
+  fastify.get("/api/user-data", async (request, reply) => {
     return { hello: "world" };
   });
 }
