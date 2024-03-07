040ed3a98cb5d5d56a69e9c55fbb7b2f182f8d1e
Nathan Gilbert <nathan.gilbert@gmail.com>
2024-03-07 12:43:34 -0700

    working cache

--- routes.js
+++ routes.js
@@ -2,17 +2,44 @@ const fs = require("node:fs");
 
 function readData() {
   try {
-    const data = fs.readFileSync("data.txt", "utf8");
-    console.log(data);
+    //const data = fs.readFileSync("data.txt", "utf8");
+    const data = "Hello, World!";
     return data;
   } catch (err) {
     console.error(err);
   }
 }
 
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
 async function routes(fastify, options) {
-  fastify.get("/api/user-data", async (request, reply) => {
+  fastify.get("/api/user-data", async (_, reply) => {
     const userData = readData();
+    return { data: userData };
+  });
+
+  fastify.get("/api/cached-user-data", async (_, reply) => {
+    const { redis } = fastify;
+    const data = await redis.get("user-data2", (err, val) => {
+      if (val) {
+        console.log("cached data", val);
+        return { data: val };
+      }
+      return null;
+    });
+
+    if (data) {
+      console.log("cached data outer", data);
+      return data;
+    }
+
+    const userData = readData();
+    await sleep(10000);
+    redis.set("user-data2", userData);
     return { data: userData };
   });
 }
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const fastify = require("fastify")({
   logger: true,
 });
 
+fastify.register(require("@fastify/redis"), { host: "127.0.0.1" });
+
 fastify.register(require("./routes"));
 
 fastify.listen({ port: 8888 }, function (err, address) {
